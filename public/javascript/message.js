d1dd8ec69f6ba42509d19f02fd84927cbedc49cc
Jacob Wallace <jacob.wallace@utexas.edu>
2017-09-11 13:59:54 -0500

    real time messaging system working on front-end

--- public/javascript/message.js
+++ public/javascript/message.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
 
+  $("#messageEnvironment").hide();
+
   var configFb = config;
 
   firebase.initializeApp(configFb);
 
-  var dataRef = firebase.database();
+  var database = firebase.database();
+
+  var messageRef = database.ref("messages");
+  //messages will be equivalent to channell reference i.e. b4LqRz
 
   var globalName;
+
+  //messageRef.onDisconnect().remove();
 
   $("#submit-name").on("click", function(){
     if ($("#user-name").val() === "")
@@ -15,7 +22,34 @@ $(document).ready(function() {
       //hide this, show the rest of the stuff
       globalName = $("#user-name").val();
       console.log(globalName);
+      $("#nameEnter").hide();
+      $("#messageEnvironment").show();
     }
-  })
+  });
+
+  $("#messageInput").keyup(function(event){
+    if(event.keyCode == 13){
+        submitThatIsh();
+    }
+  });
+
+  $("#submit-message").on("click", submitThatIsh());
+
+  function submitThatIsh() {
+    if ($("#messageInput").val() === "")
+      alert("You must enter a message to submit a message!");
+    else {
+      messageRef.set({
+        message: `${globalName}: ${$("#messageInput").val()}`
+      });
+      $("#messageInput").val("");
+      $("#recentMessages").animate({scrollTop: 0});
+    }
+  }
+
+messageRef.on("value", function(snapshot) {
+  $("#recentMessages").prepend(`<p>${snapshot.val().message}</p>`);
+  //messageRef.onDisconnect().remove();
+});
 
 });
